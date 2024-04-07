e2235794172be412cb9a62ff074b7a94ea55397e
Célio Gomes <124841986+celiogomesdelima@users.noreply.github.com>
2024-04-07 20:43:50 -0300

    Debug para True

--- cloneflix/settings.py
+++ cloneflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&$aic-i_*&l)8%qgy)+(j5^bq7)el_p#$gdq1&*s4+-4$d6=&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
